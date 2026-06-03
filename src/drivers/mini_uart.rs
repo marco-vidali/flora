0f5e35db4d091e6e5695100f12e58644849e7e66
Marco Vidali <marcovidali@outlook.com>
2026-06-03 19:35:42 +0200

    feat(mini_uart): implement init function

---
+++ src/drivers/mini_uart.rs
@@ -0,0 +1,43 @@
+use core::ptr;
+
+use crate::drivers::{
+    MMIO_BASE,
+    gpio::{GPIOPinFunc, Gpio},
+};
+
+const AUX_BASE: usize = MMIO_BASE + 0x21_5000;
+
+const AUX_ENABLES: *mut u32 = (AUX_BASE + 0x04) as *mut u32;
+const AUX_MU_IO: *mut u32 = (AUX_BASE + 0x40) as *mut u32;
+const AUX_MU_IER: *mut u32 = (AUX_BASE + 0x44) as *mut u32;
+const AUX_MU_LCR: *mut u32 = (AUX_BASE + 0x4c) as *mut u32;
+const AUX_MU_MCR: *mut u32 = (AUX_BASE + 0x50) as *mut u32;
+const AUX_MU_LSR: *mut u32 = (AUX_BASE + 0x54) as *mut u32;
+const AUX_MU_CNTL: *mut u32 = (AUX_BASE + 0x60) as *mut u32;
+const AUX_MU_BAUD: *mut u32 = (AUX_BASE + 0x68) as *mut u32;
+
+const TXD_PIN: u8 = 14;
+const RXD_PIN: u8 = 15;
+
+pub struct MiniUart;
+
+impl MiniUart {
+    pub fn init(&self, gpio: &Gpio) {
+        // configure pins to use mini uart
+        gpio.set_pin_func(TXD_PIN, GPIOPinFunc::Alt5);
+        gpio.set_pin_func(RXD_PIN, GPIOPinFunc::Alt5);
+
+        gpio.enable_pin(TXD_PIN);
+        gpio.enable_pin(RXD_PIN);
+
+        unsafe {
+            ptr::write_volatile(AUX_ENABLES, 1); // enable mini uart peripheral
+            ptr::write_volatile(AUX_MU_CNTL, 0); // disable txd and rxd
+            ptr::write_volatile(AUX_MU_IER, 0); // disable interrupts
+            ptr::write_volatile(AUX_MU_LCR, 1); // set data size to 8-bit mode
+            ptr::write_volatile(AUX_MU_MCR, 3); // set modem control to normal
+            ptr::write_volatile(AUX_MU_BAUD, 541); // set baud rate (115200 baud)
+            ptr::write_volatile(AUX_MU_CNTL, 0b11); // re-enable txd and rxd
+        }
+    }
+}
--- src/drivers/mod.rs
+++ src/drivers/mod.rs
@@ -1,3 +1,4 @@
 mod gpio;
+mod mini_uart;
 
 const MMIO_BASE: usize = 0xFE00_0000;
