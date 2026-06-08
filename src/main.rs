238a5aa390269206153186700fd64e9fd31969d9
Marco Vidali <marcovidali@outlook.com>
2026-06-08 18:27:18 +0200

    feat(main): add mini uart send example

--- src/main.rs
+++ src/main.rs
@@ -3,8 +3,14 @@
 
 use core::{hint, panic::PanicInfo};
 
+use flora::drivers::mini_uart::MiniUart;
+
 #[unsafe(no_mangle)]
 pub extern "C" fn kernel_main() -> ! {
+    let mini_uart = MiniUart::new();
+
+    mini_uart.send_str("Welcome to flora.");
+
     loop {
         hint::spin_loop(); // wait without overheating
     }
